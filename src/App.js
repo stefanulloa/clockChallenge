885ce76e6ada360322de53db011700d3bd6a8776
Stefano Ulloa <44326066+stefanulloa@users.noreply.github.com>
2020-01-29 21:55:56 +0100

    reshaping project hierarchies

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+import Displayer from "./components/Displayer";
+
+class App extends Component {
+  render() {
+    return <Displayer />;
+  }
+}
+
+export default App;
---
+++ src/components/Displayer.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class Displayer extends Component {
+  render() {
+    return <div className="Displayer" />;
+  }
+}
+
+export default Displayer;
