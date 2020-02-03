7ffc6cff54ddc3c43c00f71081e26de07f98e7de
Stefano Ulloa <44326066+stefanulloa@users.noreply.github.com>
2020-02-03 21:58:45 +0100

    comp. updates itself every second and shows time

--- src/components/Displayer.js
+++ src/components/Displayer.js
@@ -1,8 +1,29 @@
 import React, { Component } from "react";
 
 class Displayer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentTime: new Date().toLocaleTimeString()
+    };
+  }
+
+  componentDidMount() {
+    setInterval(() => this.updateTime(), 1000);
+  }
+
+  updateTime() {
+    this.setState({
+      currentTime: new Date().toLocaleTimeString()
+    });
+  }
+
   render() {
-    return <div className="Displayer" />;
+    return (
+      <div className="Displayer">
+        <h1>Time: {this.state.currentTime}</h1>
+      </div>
+    );
   }
 }
 
